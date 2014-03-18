981de80e20778b9fa752ccf96c33e0a61f128629
Troy76 <tteodoro0706@icloud.com>
2014-03-18 18:05:06 +0000

    Checked guess the secret number and return a messeage

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,4 +1,10 @@
 puts "Welcome to guess the number!"
 puts"Game was created by Troy T."
-secret_number = rand(-11)
-puts secret_number
+secret_number = rand(20)
+puts "What do you think the number is? The number is between 0 and 20"
+guess = gets.chomp.to_i
+if guess == secret_number
+  puts "correct! Thank goodness you actually got the right number"
+else 
+  puts "Wrong You suck the number was #{secret_number}"
+end
