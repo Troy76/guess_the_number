6681fe053f5b07eb113303a2f843a5bb3e20dd95
Troy76 <tteodoro0706@icloud.com>
2014-03-19 17:59:29 +0000

    Changed the if statement to an until block

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -3,8 +3,8 @@ puts"Game was created by Troy T."
 secret_number = rand(20)
 puts "What do you think the number is? The number is between 0 and 20"
 guess = gets.chomp.to_i
-if guess == secret_number
-  puts "correct! Thank goodness you actually got the right number"
-else 
-  puts "Wrong You suck the number was #{secret_number}"
+until guess == secret_number
+puts "Wrong."
+  guess = gets.chomp.to_i
 end
+puts "Congrats"
