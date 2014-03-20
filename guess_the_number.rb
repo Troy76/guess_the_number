3c2449f7beee7665c6d06db97d7f70ef0f49b76f
Troy76 <tteodoro0706@icloud.com>
2014-03-20 17:49:00 +0000

    Trying to add hints(expiremental)

--- guess_the_number.rb
+++ guess_the_number.rb
@@ -1,10 +1,19 @@
 puts "Welcome to guess the number!"
 puts"Game was created by Troy T."
-secret_number = rand(20)
-puts "What do you think the number is? The number is between 0 and 20"
+secret_number = rand(6)
+puts "What do you think the number is? The number is between 0 and 5"
 guess = gets.chomp.to_i
-until guess == secret_number
-puts "Wrong."
-  guess = gets.chomp.to_i
+if guess == 7600
+  case secret_number
+    when 1 then puts "The number is a number that is mistaken with l"
+    when 2 then puts "Double the number that starts"
+    when 3 then puts "The first oddball ever"
+    when 4 then puts "Double the number that you double when you start the line"
+    when 5 then puts "The number is the number that if put in a number line with intervals of 1 the median would be one of a kind in pi. "
+  end
+else
+  until guess == secret_number
+    puts "Wrong."
+    guess = gets.chomp.to_i
+  end
 end
-puts "Congrats"
